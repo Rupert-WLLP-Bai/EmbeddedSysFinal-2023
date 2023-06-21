0aee4f09be466437c172615aa2544482daf1a73c
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2023-06-21 19:07:12 +0800

    inital commit

---
+++ LSTM.h
@@ -0,0 +1,57 @@
+// file: LSTM.h
+
+#ifndef LSTM_H
+#define LSTM_H
+
+#include <vector>
+
+class LSTM {
+public:
+  LSTM(int inputSize, int hiddenSize);
+  void forward(const std::vector<double> &input);
+  void backward(const std::vector<double> &dOutput);
+  void updateWeights(double learningRate);
+  std::vector<double> getOutput() const;
+
+private:
+  int inputSize;
+  int hiddenSize;
+  std::vector<double> input;
+  std::vector<double> output;
+  std::vector<double> cellState;
+  std::vector<double> hiddenState;
+
+  std::vector<double> inputGateWeights;
+  std::vector<double> forgetGateWeights;
+  std::vector<double> outputGateWeights;
+  std::vector<double> cellStateWeights;
+
+  std::vector<double> inputGateBiases;
+  std::vector<double> forgetGateBiases;
+  std::vector<double> outputGateBiases;
+  std::vector<double> cellStateBiases;
+
+  std::vector<double> dInputGateWeights;
+  std::vector<double> dForgetGateWeights;
+  std::vector<double> dOutputGateWeights;
+  std::vector<double> dCellStateWeights;
+
+  std::vector<double> dInputGateBiases;
+  std::vector<double> dForgetGateBiases;
+  std::vector<double> dOutputGateBiases;
+  std::vector<double> dCellStateBiases;
+
+  std::vector<double> dHiddenState;
+  std::vector<double> dCellState;
+
+  std::vector<double> inputGate;
+  std::vector<double> forgetGate;
+  std::vector<double> outputGate;
+  std::vector<double> cellStateUpdate;
+
+  void sigmoid(std::vector<double> &x);
+  void tanh(std::vector<double> &x);
+  double dotProduct(const std::vector<double> &a, const std::vector<double> &b);
+};
+
+#endif
---
+++ benchmark.cpp
@@ -0,0 +1,29 @@
+#include <benchmark/benchmark.h>
+#include "LSTM.h"
+
+// 创建一个全局变量作为 LSTM 类的实例
+const int inputSize = 10;
+const int hiddenSize = 20;
+LSTM lstm(inputSize, hiddenSize);
+
+// 定义输入
+std::vector<double> input(inputSize, 0.5);
+
+// 定义一个基准测试函数
+// 在这个函数中，我们将多次调用 lstm.forward()、lstm.backward() 和 lstm.updateWeights()
+// 函数的运行时间将被 Google Benchmark 库记录和报告
+static void LSTM_Benchmark(benchmark::State& state) {
+    // 这个循环会运行多次，直到时间足够长以获取精确的基准测试结果
+    for (auto _ : state) {
+        lstm.forward(input);
+        lstm.backward(input);
+        lstm.updateWeights(0.01);
+    }
+}
+
+// 注册刚才定义的基准测试函数，BENCHMARK 是 Google Benchmark 库的一个宏
+// 这行代码的意思是将 LSTM_Benchmark 函数注册为一个基准测试
+BENCHMARK(LSTM_Benchmark);
+
+// 程序的主函数，它将运行所有注册的基准测试
+BENCHMARK_MAIN();
